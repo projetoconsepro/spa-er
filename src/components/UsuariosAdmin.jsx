5ea14235939aaded3ee62d2635c8c0d681b0255d
WendelGustavo <wendelfi66@gmail.com>
2023-04-19 15:48:02 -0300

    :construction: feat:Adicionar usuário pro admin

--- src/components/UsuariosAdmin.jsx
+++ src/components/UsuariosAdmin.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { React, useEffect, useState } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaUserPlus } from 'react-icons/fa'
 import { GrDocumentConfig } from 'react-icons/gr'
+import { BiEdit } from 'react-icons/bi'
 import ScrollTopArrow from './ScrollTopArrow'
 import Swal from 'sweetalert2'
 
@@ -16,14 +17,9 @@ const UsuariosAdmin = () => {
 
     useEffect(() => {
     const newData = data2.filter((item) => {
-        const searchTerm = nome.toLowerCase();
-        const itemName = item.nome.toLowerCase();
-        for(let i = 0; i < searchTerm.length; i++){
-    if(itemName[i] !== searchTerm[i]){
-      return false;
-    }
-    }
-  return true;
+       if(item.nome.toLowerCase().includes(nome.toLowerCase())){
+           return item
+       }
     })
     setData(newData)
     }, [nome])
@@ -141,101 +137,160 @@ const UsuariosAdmin = () => {
         Swal.fire({
             title: 'Informações do usuário',
             html: `<p><b>Nome:</b> ${item.nome}</p>
-                   <p><b>Email:</b> ${item.email}</p>
+                   <p><b>Email:</b> ${item.email === null ? "Email não cadastrado" : item.email}</p>
                    <p><b>Telefone:</b> ${item.telefone}</p>
                    <p><b>Perfil:</b> ${item.perfil}</p>
-                   <p><b>Status:</b> ${item.ativo === 'S' ? 'Ativo' : 'Não ativo'}</p>`,
+                   <p><b>Status:</b> ${item.ativo === 'S' ? 'Ativado' : 'Desativado'}</p>`,
             background: item.ativo === 'S' ? '#fff' : '#f8d7da',
             showCancelButton: true,
             confirmButtonText: 'Editar',
             cancelButtonText: 'Fechar',
             showLoaderOnConfirm: true,
-            preConfirm: () => {
+            preConfirm: (result) => {
+                if(result.isConfirmed){
+                    
+                }
+                else{
+                    editaUsuario(item, index)
+                }
             },
-            allowOutsideClick: () => !Swal.isLoading(),
-            didClose: () => {
-                Swal.fire({
-                    title: 'Editar informações do usuário',
-                    html: `
-                    <div className="form-group">
-                    <label for="nome" class="form-label">Nome:</label>
-                            <input id="swal-input1" class="swal2-input" value="${item.nome}">
-                            </div>
-                            <div className="form-group">
-                            <label for="email" class="form-label">Email:</label>
-                           <input id="swal-input2" class="swal2-input" value="${item.email}">
-                           </div>
-                           <div className="form-group">
-                           <label for="telefone" class="form-label">Telefone:</label>
-                           <input id="swal-input3" class="swal2-input" value="${item.telefone}">
-                           </div>
-                           <label for="perfil" class="form-label">Perfil:</label>
-                           <input id="swal-input4" class="swal2-input" value="${item.perfil}">
-                           <div className="form-group">
-                           <label for="status" class="form-label">Status:</label>
-                           <select id="swal-input" class="swal2-input">
-                               <option value="S" ${item.ativo === 'S' ? 'selected' : ''}>Ativo</option>
-                               <option value="N" ${item.ativo === 'N' ? 'selected' : ''}>Não ativo</option>
-                           </select>
-                           </div>`,
-                    background: item.ativo === 'S' ? '#fff' : '#f8d7da',
-                    showCancelButton: true,
-                    confirmButtonText: 'Salvar',
-                    cancelButtonText: 'Cancelar',
-                    showLoaderOnConfirm: true,
-                    preConfirm: () => {
-                        const nome2 = document.getElementById('swal-input1').value;
-                        const email = document.getElementById('swal-input2').value;
-                        const telefone = document.getElementById('swal-input3').value;
-                        const perfil = document.getElementById('swal-input4').value;
-                        const ativo = document.getElementById('swal-input').value === 'S' ? 'S' : 'N';
-                        const requisicao = axios.create({
-                            baseURL: process.env.REACT_APP_HOST,
-                            headers: {
-                              token: token,
-                              id_usuario: user2.id_usuario,
-                              perfil_usuario: user2.perfil[0],
-                            },
-                          });
-                          requisicao.put('/usuario', {
-                            id_usuario: item.id_usuario,
-                            nome: nome2,
-                            email: email,
-                            telefone: telefone,
-                            ativo: ativo,
-                            perfil: perfil,
-                            }).then((response) => {
-                                data3[index] = {ativo: ativo, email: email, id_usuario: item.id_usuario, nome: nome2, perfil: perfil, telefone: telefone}
-                                console.log(data3[index])
-                                setData3([...data])
-                                Swal.fire({
-                                    title: 'Sucesso!',
-                                    text: 'Usuário editado com sucesso!',
-                                    icon: 'success',
-                                    confirmButtonText: 'Ok'
-                                })
-                            }).catch((error) => {
-                                Swal.fire({
-                                    title: 'Erro!',
-                                    text: 'Erro ao editar usuário!',
-                                    icon: 'error',
-                                    confirmButtonText: 'Ok'
-                                })
-                            })
-                       
-                    },
-                    allowOutsideClick: () => !Swal.isLoading()
-                });
-            }
         });
             
         
     }
 
+    const AdicionarNovoUsuario = () => {
+        Swal.fire({
+            title: 'Adicionar novo usuario',
+            html: `
+            <div className="form-group">
+                    <label for="nome" class="form-label col-3 fs-6">Nome:</label>
+                    <input id="swal-input1" class="swal2-input" value="">
+                    </div>
+                    <div className="form-group">
+                    <label for="email" class="form-label col-3 fs-6">Email:</label>
+                   <input id="swal-input2" class="swal2-input" value="">
+                   </div>
+                   <div className="form-group">
+                   <label for="telefone" class="form-label col-3 fs-6">Telefone:</label>
+                   <input id="swal-input3" class="swal2-input" value="">
+                   </div>
+                   <div className="form-group">
+                   <label for="perfil" class="form-label col-3 fs-6">Perfil:</label>
+                   <input id="swal-input4" class="swal2-input" value="">
+                   </div>
+                   <div className="form-group">
+                   <span for="perfil" class="form-label col-3 fs-6">CPF/CNPJ:</span>
+                   <input id="swal-input4" class="swal2-input" value="">
+                   </div>
+            </div>`,
+            showCancelButton: true,
+            confirmButtonText: 'Salvar',
+            cancelButtonText: 'Cancelar',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                const nome2 = document.getElementById('swal-input1').value;
+                const email = document.getElementById('swal-input2').value;
+                const telefone = document.getElementById('swal-input3').value;
+                const perfil = document.getElementById('swal-input4').value;
+                const requisicao = axios.create({
+                    baseURL: process.env.REACT_APP_HOST,
+                    headers: {
+                      token: token,
+                      id_usuario: user2.id_usuario,
+                      perfil_usuario: user2.perfil[0],
+                    },
+                  });
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        });
+    }
+
+    const editaUsuario = async (item, index) => {
+        Swal.fire({
+            title: 'Editar informações do usuário',
+            html: `
+            <div className="form-group">
+                    <label for="nome" class="form-label col-3 fs-6">Nome:</label>
+                    <input id="swal-input1" class="swal2-input" value="${item.nome}">
+                    </div>
+                    <div className="form-group">
+                    <label for="email" class="form-label col-3 fs-6">Email:</label>
+                   <input id="swal-input2" class="swal2-input" value="${item.email}">
+                   </div>
+                   <div className="form-group">
+                   <label for="telefone" class="form-label col-3 fs-6">Telefone:</label>
+                   <input id="swal-input3" class="swal2-input" value="${item.telefone}">
+                   </div>
+                   <label for="perfil" class="form-label col-3 fs-6">Perfil:</label>
+                   <input id="swal-input4" class="swal2-input" disabled value="${item.perfil}">
+                   <div className="form-group">
+                   <label for="status" class="form-label col-3 fs-6">Status:</label>
+                   <select id="swal-input5" class="swal2-input">
+                       <option value="S" ${item.ativo === 'S' ? 'selected' : ''}>Ativo</option>
+                       <option value="N" ${item.ativo === 'N' ? 'selected' : ''}>Não ativo</option>
+                   </select>
+                   </div>`,
+            background: item.ativo === 'S' ? '#fff' : '#f8d7da',
+            showCancelButton: true,
+            confirmButtonText: 'Salvar',
+            cancelButtonText: 'Cancelar',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                const nome2 = document.getElementById('swal-input1').value;
+                const email = document.getElementById('swal-input2').value;
+                const telefone = document.getElementById('swal-input3').value;
+                const perfil = document.getElementById('swal-input4').value;
+                const ativo = document.getElementById('swal-input5').value === 'S' ? 'S' : 'N';
+                const requisicao = axios.create({
+                    baseURL: process.env.REACT_APP_HOST,
+                    headers: {
+                      token: token,
+                      id_usuario: user2.id_usuario,
+                      perfil_usuario: user2.perfil[0],
+                    },
+                  });
+                  requisicao.put('/usuario', {
+                    id_usuario: item.id_usuario,
+                    nome: nome2,
+                    email: email,
+                    telefone: telefone,
+                    ativo: ativo,
+                    perfil: perfil,
+                    }).then((response) => {
+                        data3[index] = {ativo: ativo, email: email, id_usuario: item.id_usuario, nome: nome2, perfil: perfil, telefone: telefone}
+                        console.log(data3[index])
+                        setData3([...data])
+                        Swal.fire({
+                            title: 'Sucesso!',
+                            text: 'Usuário editado com sucesso!',
+                            icon: 'success',
+                            confirmButtonText: 'Ok'
+                        })
+                    }).catch((error) => {
+                        Swal.fire({
+                            title: 'Erro!',
+                            text: 'Erro ao editar usuário!',
+                            icon: 'error',
+                            confirmButtonText: 'Ok'
+                        })
+                    })
+               
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        });
+    }
+
   return (
     <div className="dashboard-container mb-5">
     <div className="row">
+        <div className="col-7">
     <h6 className="text-start mx-4 mb-4">Usuários</h6>
+    </div>
+    <div className="col-5" id="adicionarUsuario">
+    <FaUserPlus id="iconeAddUsuario" color='#3a58c8'  size={23} onClick={()=>{AdicionarNovoUsuario()}}/>
+    </div>
+
         <div className="col-12 col-xl-8">
             <div className="row">
                 <div className="col-12 mb-4">
@@ -246,7 +301,7 @@ const UsuariosAdmin = () => {
                         </div>
 
                         <div className="col-6 align-middle" onChange={() => {filtroSelect()}}>
-                        <select className="form-select form-select-lg mb-3 mt-2" aria-label=".form-select-lg example" id="setoresSelect">
+                        <select className="form-select form-select-sm mb-3 mt-3" aria-label=".form-select-lg example" id="setoresSelect">
                                 <option value='1'>Filtro </option>
                                 <option value='perfil'>Perfil </option>
                                 <option value='ativo'>Status </option>
@@ -259,21 +314,25 @@ const UsuariosAdmin = () => {
                             <table className="table align-items-center table-flush">
                                 <thead className="thead-light">
                                     <tr>
-                                        <th className="border-bottom" scope="col">Nome</th>
-                                        <th className="border-bottom" scope="col">Telefone</th>
-                                        <th className="border-bottom" scope="col">Perfil</th>
-                                        <th className="border-bottom" scope="col">   ‎‎</th>
+                                        <th className="border-bottom" id="tabelaUsuarios" scope="col">Nome</th>
+                                        <th className="border-bottom" id="tabelaUsuarios" scope="col">Telefone</th>
+                                        <th className="border-bottom" id="tabelaUsuarios2" scope="col">Email</th>
+                                        <th className="border-bottom" id="tabelaUsuarios" scope="col">Perfil</th>
+                                        <th className="border-bottom" id="tabelaUsuarios2" scope="col">Status</th>
+                                        <th className="border-bottom" scope="col">‎‎</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {data.map((item, index) => (
-                                        <tr className="card-list" key={index} onClick={() => {informacoes(item)}}>
-                                            <th className="fw-bolder col-5" scope="row" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}> 
+                                        <tr className="card-list" key={index}>
+                                            <th className="fw-bolder col" scope="row" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}  onClick={() => {informacoes(item)}}> 
                                             {item.nome.length > 14 ? item.nome.substring(0, 14) + "..." : item.nome}
                                              </th>
-                                            <td className="fw-bolder col-4" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}> <small> {item.telefone} </small></td>
-                                            <td className="fw-bolder col-2" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}> <small> {item.perfil} </small></td>
-                                            <td className="fw-bolder col-1" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}> <small><GrDocumentConfig /></small></td>
+                                            <td className="fw-bolder col" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}  onClick={() => {informacoes(item)}}> <small> {item.telefone} </small></td>
+                                            <td className="fw-bolder col" id="tabelaUsuarios2" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}  onClick={() => {informacoes(item)}}> <small> {item.email} </small></td>
+                                            <td className="fw-bolder col" id="tabelaUsuarios" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}  onClick={() => {informacoes(item)}}> <small> {item.perfil} </small></td>
+                                            <td className="fw-bolder col" id="tabelaUsuarios2" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }}  onClick={() => {informacoes(item)}}> <small> {item.ativo === 'S' ? 'Ativado' : 'Desativado'} </small></td>
+                                            <td className="fw-bolder col" id="tabelaUsuarios3" style={{ backgroundColor: item.ativo === 'S' ? '#fff' : '#F8D7DA' }} onClick={()=>{editaUsuario(item , index)}}> <small><BiEdit size={20} /></small></td>
                                         </tr>
                                     ))}
                                 </tbody>
