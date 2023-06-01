023fb7f6c2a5003bbff638c3af677486d83a6486
WendelGustavo <wendelfi66@gmail.com>
2023-06-01 16:41:19 -0300

    :bug: fix:Correção de bug
    Co-authored-by: vinikrummenauer <viniciuskdepaula@gmail.com>

--- src/components/NewPassword.jsx
+++ src/components/NewPassword.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
 import sha256 from 'crypto-js/sha256';
 import FuncTrocaComp from '../util/FuncTrocaComp';
 
