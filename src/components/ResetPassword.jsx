5a6a95545455265c0c913e654afe19cda96ca6d4
Vinicius Krummenauer <viniciuskdepaula@gmail.com>
2023-06-02 16:06:43 -0300

    :bug: fix:Correções de bugs

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -203,20 +203,6 @@ const ResetPassword = () => {
                 <button type="submit" className="btn2 botao" onClick={() => {voltarPagina()}}>Voltar</button>
                 <button onClick={handleSubmit} className="btn3 botao">
                   {sucesso ? "Gerar código " : "Avançar "}
-                  <span className="align-self-end">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-right"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                      />
-                    </svg>
-                  </span>
                 </button>
               </div>
               <div
