cd1e02719fb102376e22edef2cd39a3471f73b6b
WendelGustavo <wendelfi66@gmail.com>
2023-07-07 16:23:31 -0300

    :construction: feat: Transferencia credito parceiro

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 import { AuthContext } from "../pages/contexts/auth";
 import Swal from 'sweetalert2'
 import { cpf, cnpj } from 'cpf-cnpj-validator';
