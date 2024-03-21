8df532dc5cf25aeb31cd3c138265ca5382c1cf7f
WendelGustavo <wendelfi66@gmail.com>
2024-03-21 11:47:15 -0300

    :recycle: refactor: Implementada a lista de Modelos / Fabricantes

--- src/components/AdicionarModelo.jsx
+++ src/components/AdicionarModelo.jsx
@@ -8,11 +8,15 @@ import {
   Select,
   Text,
   Checkbox,
+  Pagination,
 } from "@mantine/core";
-import { IconChevronRight } from "@tabler/icons-react";
+import { IconChevronRight, IconPlus } from "@tabler/icons-react";
 import Swal from "sweetalert2";
 import createAPI from "../services/createAPI";
 import { FaCarAlt } from "react-icons/fa";
+import Filtro from "../util/Filtro";
+import CarroLoading from "./Carregamento";
+import VoltarComponente from "../util/VoltarComponente";
 
 const AdicionarModelo = () => {
   const [estado, setEstado] = useState(false);
@@ -22,6 +26,22 @@ const AdicionarModelo = () => {
   const [fabricantesSelect, setFabricantesSelect] = useState([]);
   const [modelo, setModelo] = useState("");
   const [checked, setChecked] = useState(false);
+  const [step, setStep] = useState(1);
+  const [estadoLoading, setEstadoLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fabricanteModelo, setFabricanteModelo] = useState("");
+  const itemsPerPage = 50;
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = fabricanteModelo.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   const getFabricantes = () => {
     const requisicao = createAPI();
@@ -51,6 +71,7 @@ const AdicionarModelo = () => {
   };
 
   useEffect(() => {
+    handleRequest();
     getFabricantes();
   }, []);
 
@@ -75,6 +96,8 @@ const AdicionarModelo = () => {
               setMensagem("");
               setModelo("");
               getFabricantes();
+              handleRequest();
+              setStep(1);
               Swal.fire({
                 icon: "success",
                 title: "Sucesso!",
@@ -153,6 +176,62 @@ const AdicionarModelo = () => {
     }
   };
 
+  const handleConsultaSelected = (consulta) => {
+    handleRequest(consulta);
+  };
+
+  const handleRequest = (query) => {
+    setEstadoLoading(true);
+    const requisicao = createAPI();
+
+    let url = `veiculo/fabricantes/modelos/`;
+
+    if (query !== undefined) {
+      const base64 = btoa(query);
+
+      url = `veiculo/fabricantes/modelos/?query=${base64}`;
+    }
+
+    requisicao
+      .get(url)
+      .then((response) => {
+        if (response.data.msg.resultado) {
+          setMensagem("");
+          setEstado(false);
+          const newData = response?.data?.data?.map((item) => ({
+            fabricante: item.fabricante,
+            modelo: item.modelo,
+          }));
+
+          setFabricanteModelo(newData);
+
+          setEstadoLoading(false);
+        } else {
+          setFabricanteModelo([]);
+          setEstado(true);
+          setMensagem(response.data.msg.msg);
+          setEstadoLoading(false);
+        }
+      })
+      .catch(function (error) {
+        console.log(error?.response?.data);
+        if (
+          error?.response?.data?.msg === "Cabeçalho inválido!" ||
+          error?.response?.data?.msg === "Token inválido!" ||
+          error?.response?.data?.msg ===
+            "Usuário não possui o perfil mencionado!"
+        ) {
+          localStorage.removeItem("user");
+          localStorage.removeItem("token");
+          localStorage.removeItem("perfil");
+        } else {
+          setEstado(true);
+          setMensagem(error?.response?.data?.msg);
+          setEstadoLoading(false);
+        }
+      });
+  };
+
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card
@@ -162,67 +241,177 @@ const AdicionarModelo = () => {
         className="bg-admin-parceiro"
         withBorder={false}
       >
-        <Group position="apart" mt="md" mb="xs">
-          <Text weight={500}>Adicionar veículo:</Text>
-        </Group>
-        <div className="text-start">
-          <Divider my="sm" size="sm" variant="dashed" />
+        {step === 2 ? (
+          <>
+            <Group position="apart" mt="md" mb="xs">
+              <Text weight={500}>Adicionar veículo:</Text>
+            </Group>
+            <div className="text-start">
+              <Divider my="sm" size="sm" variant="dashed" />
 
-          <Checkbox
-            label="Fabricante não cadastrado"
-            onChange={(e) => setChecked(e.currentTarget.checked)}
-          />
-
-          {checked ? (
-            <Input.Wrapper label="Escolha o fabricante do veículo" mt="md">
-              <Input
-                icon={<FaCarAlt />}
-                placeholder=""
-                value={fabricante}
-                onChange={(e) => setFabricante(e.currentTarget.value)}
+              <Checkbox
+                label="Fabricante não cadastrado"
+                onChange={(e) => setChecked(e.currentTarget.checked)}
               />
-            </Input.Wrapper>
-          ) : (
-            <Select
-              label="Escolha o fabricante"
-              className="mt-4"
-              value={fabricantesSelect}
-              data={fabricantes}
-              onChange={(e) => setFabricantesSelect(e)}
-              searchable
-              nothingFound="Sem resultados"
-            />
-          )}
 
-          <Group position="left" mt="md" mb="xs">
-            <Input.Wrapper label="Escolha o modelo do veículo" mt="md">
-              <Input
-                icon={<FaCarAlt />}
-                placeholder=""
-                value={modelo}
-                onChange={(e) => setModelo(e.currentTarget.value)}
-              />
-            </Input.Wrapper>
-          </Group>
+              {checked ? (
+                <Input.Wrapper label="Escolha o fabricante do veículo" mt="md">
+                  <Input
+                    icon={<FaCarAlt />}
+                    placeholder=""
+                    value={fabricante}
+                    onChange={(e) => setFabricante(e.currentTarget.value)}
+                  />
+                </Input.Wrapper>
+              ) : (
+                <Select
+                  label="Escolha o fabricante"
+                  className="mt-4"
+                  value={fabricantesSelect}
+                  data={fabricantes}
+                  onChange={(e) => setFabricantesSelect(e)}
+                  searchable
+                  nothingFound="Sem resultados"
+                />
+              )}
 
-          <Button
-            variant="gradient"
-            gradient={{ from: "teal", to: "blue", deg: 60 }}
-            fullWidth
-            mt="md"
-            radius="md"
-            onClick={() => submit()}
-          >
-            Adicionar ‎ <IconChevronRight color="white" mt="3" size={15} />
-          </Button>
-        </div>
-        <div
-          className="alert alert-danger mt-3"
-          role="alert"
-          style={{ display: estado ? "block" : "none" }}
-        >
-          {mensagem}
-        </div>
+              <Group position="left" mt="md" mb="xs">
+                <Input.Wrapper label="Escolha o modelo do veículo" mt="md">
+                  <Input
+                    icon={<FaCarAlt />}
+                    placeholder=""
+                    value={modelo}
+                    onChange={(e) => setModelo(e.currentTarget.value)}
+                  />
+                </Input.Wrapper>
+              </Group>
+
+              <Button
+                variant="gradient"
+                gradient={{ from: "teal", to: "blue", deg: 60 }}
+                fullWidth
+                mt="md"
+                radius="md"
+                onClick={() => submit()}
+              >
+                Adicionar ‎ <IconChevronRight color="white" mt="3" size={15} />
+              </Button>
+            </div>
+            <div
+              className="alert alert-danger mt-3"
+              role="alert"
+              style={{ display: estado ? "block" : "none" }}
+            >
+              {mensagem}
+            </div>
+            <Button
+              className={"bg-gray-500 mx-2"}
+              size="md"
+              mt={40}
+              radius="md"
+              onClick={() => {
+                setStep(1);
+              }}
+            >
+              Voltar
+            </Button>
+          </>
+        ) : (
+          <>
+            <div className="row mb-3">
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-5 mx-1">
+                    <Filtro
+                      nome={"AdicionarModelo"}
+                      onConsultaSelected={handleConsultaSelected}
+                      onLoading={estadoLoading}
+                    />
+                  </div>
+                  <div className="col-6 text-center">
+                    <Button
+                      variant="gradient"
+                      gradient={{ from: "cyan", to: "blue", deg: 60 }}
+                      fullWidth
+                      onClick={() => setStep(2)}
+                    >
+                      <IconPlus color="white" mt="3" size={15} /> ‎ Adicionar
+                      Modelo / Fabricante
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div className="row">
+                  <div className="col-12 mb-4">
+                    <div className="card border-0 shadow">
+                      {fabricanteModelo.length === 0 && mensagem !== "Nenhum registro encontrado" ? (
+                        <div>
+                          <CarroLoading />
+                        </div>
+                      ) : (
+                        <div className="table-responsive">
+                          <table className="table align-items-center table-flush">
+                            <thead className="thead-light">
+                              <tr>
+                                <th
+                                  className="border-bottom"
+                                  id="tabelaUsuarios"
+                                  scope="col"
+                                >
+                                  Fabricante
+                                </th>
+                                <th
+                                  className="border-bottom"
+                                  id="tabelaUsuarios"
+                                  scope="col"
+                                >
+                                  Modelo
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {currentItems.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.fabricante}</td>
+                                  <td id="tabelaUsuarios">{item.modelo}</td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      )}
+                      <div
+                        className="alert alert-danger mt-4 mx-3"
+                        role="alert"
+                        style={{ display: estado ? "block" : "none" }}
+                      >
+                        {mensagem}
+                      </div>
+                    </div>
+                  </div>
+                  <Group position="center" mb="md">
+                    <Pagination
+                      value={currentPage}
+                      size="sm"
+                      onChange={handlePageChange}
+                      total={
+                        Math.floor(fabricanteModelo.length / 50) ===
+                        fabricanteModelo.length / 50
+                          ? fabricanteModelo.length / 50
+                          : Math.floor(fabricanteModelo.length / 50) + 1
+                      }
+                      limit={itemsPerPage}
+                    />
+                  </Group>
+                  <VoltarComponente />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </Card>
     </Card>
   );
