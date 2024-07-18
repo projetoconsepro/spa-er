0103433837c9ba4185c78cdc436268e2885e25bd
plovatto <pandora-blovatto@educar.rs.gov.br>
2024-07-18 15:01:38 -0300

    :recycle: refactor: Melhorias e edição de tempo

--- src/components/ListarMovimentosAdmin.jsx
+++ src/components/ListarMovimentosAdmin.jsx
@@ -3,10 +3,10 @@ import createAPI from "../services/createAPI";
 import { AiOutlineReload } from "react-icons/ai";
 import { FaEllipsisH, FaPowerOff } from "react-icons/fa";
 import Swal from "sweetalert2";
-import { Button, Group, Loader, Pagination } from "@mantine/core";
+import { Button, Group, Loader, Pagination, Modal } from "@mantine/core";
 import VoltarComponente from "../util/VoltarComponente";
 import Filtro from "../util/Filtro";
-import { RiDeleteBinFill } from "react-icons/ri";
+import { RiDeleteBinFill, RiEditLine } from "react-icons/ri";
 
 const ListarMovimentosAdmin = () => {
 
@@ -22,6 +22,11 @@ const ListarMovimentosAdmin = () => {
   const [pageFiltro, setPageFiltro] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [totalPagesFiltro, setTotalPagesFiltro] = useState(1);
+  const [ModalAberto, setModalAberto] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [loadingButton, setLoadingButton] = useState(false);
+  const [index, setindex] = useState(false);
+  const [tempoSelecionado, setTempoSelecionado] = useState('');
 
   useEffect(() => {
     const listar = async () => {
@@ -34,6 +39,7 @@ const ListarMovimentosAdmin = () => {
           setEstado2(true);
           const newData = response.data.data.map((item) => ({
             id_movimento: item.id_movimento,
+            id_credito: item.id_credito,
             hora: item.hora,
             tipo: item.tipo,
             tipo_movimento: item.tipo_movimento,
@@ -99,6 +105,7 @@ const ListarMovimentosAdmin = () => {
           setEstado(false);
           const newData = response.data.data.map((item) => ({
             id_movimento: item.id_movimento,
+            id_credito: item.id_credito,
             hora: item.hora,
             tipo_movimento: item.tipo_movimento,
             nome_usuario: item.nome_usuario,
@@ -247,19 +254,111 @@ const ListarMovimentosAdmin = () => {
     cancelamento: 'Cancelamento',
     infracao: 'Infração',
     saida: 'Saída',
+  }; 
+
+  const editarTempo = (index, id, tempo) => {
+    setLoadingButton(true)
+    const requisicao = createAPI();
+    requisicao
+      .put(`/movimento`, {
+        tempo: tempo,
+        id: id
+      })
+      .then((response) => {
+        setLoadingButton(false)
+        setModalAberto(false)
+        Swal.fire(
+          "Atualizado!",
+          "O tempo do movimento foi atualizado com sucesso.",
+          "success"
+        );
+        const valorAtualizado = response.data.valor;
+        const tempoAtualizado = response.data.tempo;
+        data[index].valor = valorAtualizado;
+        data[index].tempo = tempoAtualizado;
+        setData([...data]);
+      })
+      .catch((error) => {
+        if (
+          error?.response?.data?.msg === "Cabeçalho inválido!" ||
+          error?.response?.data?.msg === "Token inválido!" ||
+          error?.response?.data?.msg === "Usuário não possui o perfil mencionado!"
+        ) {
+          localStorage.removeItem("user");
+          localStorage.removeItem("token");
+          localStorage.removeItem("perfil");
+        } else {
+          console.log(error);
+        }
+      });
   };
+
+  useEffect(() => {
+    if (selectedItem) {
+      const opcoes = ["00:30:00", "01:00:00", "01:30:00", "02:00:00"].filter(opcao => opcao !== selectedItem.tempo);
+      if (opcoes.length > 0) {
+        setTempoSelecionado(opcoes[0]);
+      }
+    }
+  }, [selectedItem]);
+
+  const abrirModalEditarTempo = (item, index) => {
+    setModalAberto(true);
+    setSelectedItem(item);
+    setindex(index)
+  };  
 
   return (
     <div className="dashboard-container mb-3">
+      <Modal
+        opened={ModalAberto}
+        onClose={() => {
+          setModalAberto(false);
+          setSelectedItem(null);
+        }}
+        centered
+        title="Editar Tempo"
+        size="md"
+        className="flex items-center justify-center"
+      >
+        {selectedItem && (
+          <div className="flex flex-col items-center justify-center">
+            <select
+              className="form-select form-select-lg mb-4 mt-5"
+              aria-label=".form-select-lg example"
+              value={tempoSelecionado}
+              onChange={(e) => setTempoSelecionado(e.target.value)}
+            >
+              {selectedItem.tempo !== "00:30:00" && <option value="00:30:00">30 Minutos</option>}
+              {selectedItem.tempo !== "01:00:00" && <option value="01:00:00">60 Minutos</option>}
+              {selectedItem.tempo !== "01:30:00" && <option value="01:30:00">90 Minutos</option>}
+              {selectedItem.tempo !== "02:00:00" && <option value="02:00:00">120 Minutos</option>}
+            </select>
+            <div className="mb-2 mt-3 gap-2 flex justify-center items-center w-full text-center">
+              <Button
+                loading={loadingButton}
+                className="bg-blue-50 m-2"
+                size="md"
+                radius="md"
+                onClick={() => editarTempo(index, selectedItem.id_credito, tempoSelecionado)}
+              >
+                Salvar
+              </Button>
+              <VoltarComponente />
+            </div>
+          </div>
+        )}
+      </Modal>
       <p className="mx-3 text-start fs-4 fw-bold">Listar Movimentos</p>
       <div className="row mb-3">
         <div className="col-12">
           <div className="row">
-            <div className="col-6 mx-2">
-              <Filtro nome={"ListarMovimentosAdmin"} onConsultaSelected={handleConsultaSelected} onLoading={estadoLoading} />
-
+            <div className="col-lg-6 col-6">
+            <Filtro nome={"ListarMovimentosAdmin"} onConsultaSelected={handleConsultaSelected} onLoading={estadoLoading} />
             </div>
-            <div className="col-2 text-end">
+            <div className="col-lg-3 col-3">
+            </div>
+            <div className="col-lg-3 col-3 text-end me-0">
               <Button
                 variant="gradient"
                 gradient={{ from: "indigo", to: "blue", deg: 60 }}
@@ -282,10 +381,10 @@ const ListarMovimentosAdmin = () => {
             <div className="col-12 mb-4">
               {estado2 ? (
                 <div className="card border-0 shadow">
-                  <div className="table-responsive">
+                  <div className="table-responsive" style={{ overflowX: 'auto' }}>
                     <table className="table align-items-center table-flush">
                       <thead className="thead-light">
-                        <tr>
+                      <tr>
                           <th
                             className="border-bottom"
                             id="tabelaUsuarios"
@@ -309,14 +408,14 @@ const ListarMovimentosAdmin = () => {
                           </th>
                           <th
                             className="border-bottom"
-                            id="tabelaUsuarios"
+                            id="tabelaUsuarios2"
                             scope="col"
                           >
                             setor
                           </th>
                           <th
                             className="border-bottom"
-                            id="tabelaUsuarios2"
+                            id="tabelaUsuarios"
                             scope="col"
                           >
                             vaga
@@ -330,7 +429,7 @@ const ListarMovimentosAdmin = () => {
                           </th>
                           <th
                             className="border-bottom"
-                            id="tabelaUsuarios2"
+                            id="tabelaUsuarios"
                             scope="col"
                           >
                             valor
@@ -344,7 +443,7 @@ const ListarMovimentosAdmin = () => {
                           </th>
                           <th
                             className="border-bottom"
-                            id="tabelaUsuarios2"
+                            id="tabelaUsuarios"
                             scope="col"
                           >
                             usuário
@@ -365,32 +464,32 @@ const ListarMovimentosAdmin = () => {
                       <tbody>
                         {data.map((item, index) => (
                           <tr key={index}>
-                            <td id="tabelaUsuarios2">{item.placa_veiculo}</td>
-                            <td id="tabelaUsuarios2">
+                            <td id="tabelaUsuarios">{item.placa_veiculo}</td>
+                            <td id="tabelaUsuarios">
                               {tipoMovimentoComAcentos[item.tipo_movimento]}
                             </td>
-                            <td id="tabelaUsuarios2">{new Date(item.hora).toLocaleString()}</td>
+                            <td id="tabelaUsuarios">{new Date(item.hora).toLocaleString()}</td>
                             <td id="tabelaUsuarios2">{item.nome_setor}</td>
-                            <td id="tabelaUsuarios2">{item.numero_vaga}</td>
+                            <td id="tabelaUsuarios">{item.numero_vaga}</td>
                             {item.tipo_movimento == 'notificacao' ? (
-                              <td id="tabelaUsuarios2" colSpan="3" style={{ fontWeight: 'medium', marginTop: '2rem', color: item.estado_notificacao === 'Cancelada' ? 'black' : item.estado_notificacao === 'Regularizada' ? '#20E300' : item.estado_notificacao === 'Pendente' ? '#E30000' : 'black' }}>    
+                              <td id="tabelaUsuarios" colSpan="3" style={{ fontWeight: 'medium', marginTop: '2rem', color: item.estado_notificacao === 'Cancelada' ? 'black' : item.estado_notificacao === 'Regularizada' ? '#20E300' : item.estado_notificacao === 'Pendente' ? '#E30000' : 'black' }}>    
                               Notificação {item.estado_notificacao}
 
                               </td>
                             ) : (
                               <>
                                 <td id="tabelaUsuarios2">{item.tipo || '...'}</td>
-                                <td id="tabelaUsuarios2">{item.valor ? `R$ ${parseFloat(item.valor).toFixed(2)}` : '...'}</td>
-                                <td id="tabelaUsuarios2">{item.tempo || '...'}</td>
+                                <td id="tabelaUsuarios">{item.valor ? `R$ ${parseFloat(item.valor).toFixed(2)}` : '...'}</td>
+                                <td id="tabelaUsuarios">{item.tempo || '...'}</td>
                               </>
                             )}
-                            <td id="tabelaUsuarios2">{item.nome_usuario}</td>
+                            <td id="tabelaUsuarios">{item.nome_usuario}</td>
                             <td id="tabelaUsuarios2">{item.perfil_usuario.charAt(0).toUpperCase() + item.perfil_usuario.slice(1)}</td>
 
                             <td className="fw-bolder col" id="tabelaUsuarios3">
-                              <div className="btn-group">
+                            <div className="btn-group">
                                 {item.estado_notificacao === "Cancelada" || item.estado_notificacao === "Regularizada" || item.tipo_movimento === "cancelamento" ? (
-                                  <FaEllipsisH />
+                                  <div></div>
                                 ) : (
                                   <button
                                     className="btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0"
@@ -401,26 +500,29 @@ const ListarMovimentosAdmin = () => {
                                     <FaEllipsisH />
                                   </button>
                                 )}
-                                <div className="dropdown-menu dashboard-dropdown align-items-center dropdown-menu-start">
+                                <div className="dropdown-menu dashboard-dropdown dropdown-menu-start mt-3 py-1">
                                   {item.tipo_movimento !== "notificacao" ? (
-                                    <h6
-                                      className="dropdown-item d-flex align-items-center justify-content-center text-danger mt-2"
-                                      onClick={() => deletar(item, index)}
-                                      style={{ padding: '0' }}
-                                    >
-                                      <RiDeleteBinFill />
-                                      ‎‎ Deletar
-                                    </h6>
-                                  ) : (
-                                    <h6
-                                      className="dropdown-item d-flex align-items-center justify-content-center text-primary mt-2"
-                                      style={{ padding: '0' }}
-                                      onClick={() => cancelar(item, index)}
-                                    >
-                                      <FaPowerOff />
-                                      ‎‎ ‎Cancelar
-                                    </h6>
-                                  )}
+                                    <div>
+                                      <h6 className="dropdown-item d-flex justify-content-center align-items-center text-danger"
+                                        onClick={() => deletar(item, index)}
+                                      >
+                                        <RiDeleteBinFill />
+                                        ‎‎ Remover {tipoMovimentoComAcentos[item.tipo_movimento]}
+                                      </h6>
+                                      {item.tempo && (
+                                        <h6 className="dropdown-item d-flex justify-content-center align-items-center text-info"
+                                        onClick={() => abrirModalEditarTempo(item, index)}>
+                                          <RiEditLine />
+                                          Editar tempo
+                                        </h6>
+                                      )}
+                                    </div>
+                                  ) : (<h6 className="dropdown-item d-flex justify-content-center align-items-center text-primary"
+                                    onClick={() => cancelar(item, index)}
+                                  >
+                                    <FaPowerOff />
+                                    ‎‎ ‎Cancelar
+                                  </h6>)}
                                 </div>
                               </div>
                             </td>
