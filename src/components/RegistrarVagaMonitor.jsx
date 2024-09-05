90f492f90e575010c64fbf94475c95d4aba3399a
Plovatto <pandora-blovatto@educar.rs.gov.br>
2024-09-05 13:23:09 -0300

    :recycle: refactor: Confirmar a impressão da via do cliente

--- src/components/RegistrarVagaMonitor.jsx
+++ src/components/RegistrarVagaMonitor.jsx
@@ -366,17 +366,27 @@ const RegistrarVagaMonitor = () => {
         .then((response) => {
           if (response.data.msg.resultado === true) {
             if (response.data.msg.msg !== "Vaga atualizada com sucesso" && response.data.data.debitado !== "S"){
-            ImpressaoTicketEstacionamento(
-              'PRIMEIRA',
-              response.data.data.chegada,
-              response.data.data.tempo,
-              response.config.headers.id_usuario,
-              vagaa,
-              tirarTraco,
-              response.data.msg.msg === "Debito" ? 'debito' : selectedButton,
-              response.data.msg.msg === "Debito" ? response.data.data.tempoDebito : tempo,
-              response.data.data.notificacao_pendente
-            );
+                Swal.fire({
+                  title: 'Deseja imprimir a via do cliente?',
+                  icon: 'question',
+                  showCancelButton: true,
+                  confirmButtonText: 'Sim',
+                  cancelButtonText: 'Não'
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    ImpressaoTicketEstacionamento(
+                      'PRIMEIRA',
+                      response.data.data.chegada,
+                      response.data.data.tempo,
+                      response.config.headers.id_usuario,
+                      vagaa,
+                      tirarTraco,
+                      response.data.msg.msg === "Debito" ? 'debito' : selectedButton,
+                      response.data.msg.msg === "Debito" ? response.data.data.tempoDebito : tempo,
+                      response.data.data.notificacao_pendente
+                    );
+                  }
+                });
             }
 
             const listaVagasString = localStorage.getItem("listaVagas");
