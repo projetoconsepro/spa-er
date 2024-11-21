08d4cc2c245e50fe82c1be94445146e62649c4f1
plovatto <pandora-blovatto@educar.rs.gov.br>
2024-11-21 16:51:12 -0300

    :recycle: refactor: Ajustando botão add saldo

--- src/components/ListarVeiculos.jsx
+++ src/components/ListarVeiculos.jsx
@@ -748,11 +748,23 @@ const ListarVeiculos = () => {
               </div>
               <div>
                 <div className="d-flex align-items-center fw-bold">
-                    <button className="mx-1 mb-5" style={{ width: '43px', height: '41px', borderRadius: '100px', background: '#3381D4', border: 'none' }}
-                     onClick={() => {
+                <button
+                    className="mb-5 p-0"
+                    style={{
+                      width: '43px',
+                      height: '41px',
+                      borderRadius: '100px',
+                      background: '#3381D4',
+                      border: 'none',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}
+                    onClick={() => {
                       FuncTrocaComp("InserirCreditos");
-                    }}>
-                    <IoMdAdd color="white" size="30px" />
+                    }}
+                  >
+                    <IoMdAdd color="white" className="p-0 m-0" size="35px" />
                   </button>
                 </div>
               </div>
